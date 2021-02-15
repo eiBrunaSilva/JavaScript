06aef851a424eb415c43b12f224d12a1323470a4
Bruna Silva <68930726+eiBrunaSilva@users.noreply.github.com>
2021-02-15 20:53:23 -0300

    Simple and Compound Conditions

---
+++ Aula004/ex001.js
@@ -0,0 +1,6 @@
+var spe = 60.5
+console.log(`Your car´s speed is ${spe}KM/H`)
+if(spe > 60){
+  console.log('You have exceeded the permitted speed. Fined!')
+}
+console.log('Always drive with seat belts!')
---
+++ Aula004/ex002.js
@@ -0,0 +1,6 @@
+var cou = 'EUA'
+if(cou == 'Brazil'){
+  console.log('Brazilian!')
+}else{
+  console.log('Foreign!')
+}
