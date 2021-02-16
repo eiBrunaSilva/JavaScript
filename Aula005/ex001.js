d17009fdb86d3cf40233dfa82ada7830279104c2
Bruna Silva <68930726+eiBrunaSilva@users.noreply.github.com>
2021-02-16 19:15:54 -0300

    Nested and Multiple Condition

---
+++ Aula005/ex001.js
@@ -0,0 +1,8 @@
+var age = 70
+if (age < 16){
+  console.log('Don´t vote!')
+} else if(age >= 16 && age <18 || age > 65) {
+    console.log('Optional vote')
+  } else {
+    console.log('Compulsory vote!')
+  }
