c64ad2615881338750939bca7196cde72c9873e0
Bruna Nunes da Silva <silvabruna.email@gmail.com>
2021-06-24 18:19:21 -0300

    js

---
+++ Alura/Bank/index.js
@@ -0,0 +1,21 @@
+class Cliente {
+  nome;
+  cpf;
+  agencia;
+  saldo;
+  rg;
+}
+
+const cliente1 = new Cliente();
+
+cliente1.nome = "Bruna";
+Cliente1.cpf = 111222000;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+cliente2.nome = "Alex";
+Cliente2.cpf = 29829828928;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+console.log(cliente1, cliente2);
